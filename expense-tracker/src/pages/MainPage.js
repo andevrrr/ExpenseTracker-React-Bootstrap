16000aad5261e5381919d3e9108d7778673499dd
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-01-09 02:38:57 +0300

    some styling

--- expense-tracker/src/pages/MainPage.js
+++ expense-tracker/src/pages/MainPage.js
@@ -69,19 +69,27 @@ const MainPage = () => {
       <div className="text-center mb-4">
         <h1 className="text-4xl font-bold">January</h1>
       </div>
-      <div className="flex justify-between">
-        <div className="w-1/2 bg-white p-6 rounded-lg shadow-lg">
+      <div className="flex justify-evenly items-center my-12">
+        <div className="w-auto bg-white p-6 rounded-lg shadow-lg">
           <canvas ref={chartRef}></canvas>
         </div>
-        <div className="w-1/2 bg-white p-6 rounded-lg shadow-lg">
-          <ul>
+        <div className="">
+          <ul className="flex flex-wrap">
             {pieChartData.labels.map((category, index) => (
               <li
                 key={category}
-                className="text-lg mb-2"
-                style={{ color: pieChartData.datasets[0].backgroundColor[index] }}
+                className="p-8 rounded-lg m-2 text-center"
+                style={{
+                  backgroundColor:
+                    pieChartData.datasets[0].backgroundColor[index],
+                  flexBasis: "calc(50% - 1rem)", // Set to 50% minus the margin
+                  maxWidth: "calc(50% - 1rem)", // Set to 50% minus the margin
+                }}
               >
-                {category}: ${pieChartData.datasets[0].data[index]}
+                <p className="text-3xl">{category}</p>
+                <p className="text-1xl mt-3">
+                  ${pieChartData.datasets[0].data[index]}
+                </p>
               </li>
             ))}
           </ul>
@@ -89,17 +97,25 @@ const MainPage = () => {
       </div>
       <div className="mt-8">
         <h2 className="text-xl font-semibold mb-4">Purchases</h2>
-        <ul>
+        <ul className="border border-gray-200 p-2 rounded-lg">
+          <li className="text-lg mb-2 flex font-semibold border-b border-gray-200">
+            <p className="flex-1">Title</p>
+            <p className="flex-1">Date</p>
+            <p className="flex-1">Category</p>
+            <p className="flex-1">Price</p>
+          </li>
           {purchaseData.map((purchase, index) => (
-            <li key={index} className="text-lg mb-2">
-              <strong>{purchase.title}</strong> - {purchase.date} -{" "}
-              {purchase.price} - {purchase.category}
+            <li
+              key={index}
+              className="text-lg mb-2 flex items-center justify-between"
+            >
+              <p className="flex-1">{purchase.title}</p>
+              <p className="flex-1">{purchase.date}</p>
+              <p className="flex-1">{purchase.category}</p>
+              <p className="flex-1">{purchase.price}</p>
             </li>
           ))}
         </ul>
-      </div>
-      <div className="mt-8 text-center text-gray-600">
-        <p>bottom</p>
       </div>
     </div>
   );
