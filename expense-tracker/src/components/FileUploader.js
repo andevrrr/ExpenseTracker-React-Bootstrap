f8f187ccf6383f910b4a2d3b933179e93985b74e
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-02-03 13:47:32 +0200

    added isLoading

--- expense-tracker/src/components/FileUploader.js
+++ expense-tracker/src/components/FileUploader.js
@@ -3,7 +3,11 @@ import { Button } from "react-bootstrap";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const FileUploader = ({ onFileSelectSuccess, onFileSelectError }) => {
+const FileUploader = ({
+  onFileSelectSuccess,
+  onFileSelectError,
+  onFileUploadStart,
+}) => {
   const [selectedFile, setSelectedFile] = useState(null);
 
   const handleFileChange = (event) => {
@@ -14,22 +18,24 @@ const FileUploader = ({ onFileSelectSuccess, onFileSelectError }) => {
   };
 
   const handleFileUpload = () => {
-    const formData = new FormData();
-    formData.append("file", selectedFile);
-
-    axios
-      .post("http://localhost:3000/upload", formData, {
-        withCredentials: true,
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      })
-      .then((response) => {
-        onFileSelectSuccess(selectedFile);
-      })
-      .catch((error) => {
-        onFileSelectError(error.message);
-      });
+    if (selectedFile) {
+      onFileUploadStart();
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+      axios
+        .post("http://localhost:3000/upload", formData, {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((response) => {
+          onFileSelectSuccess(selectedFile);
+        })
+        .catch((error) => {
+          onFileSelectError(error.message);
+        });
+    }
   };
 
   return (
@@ -38,10 +44,10 @@ const FileUploader = ({ onFileSelectSuccess, onFileSelectError }) => {
         type="file"
         onChange={handleFileChange}
         className="mb-4 file:mr-4 file:py-2 file:px-4
-                           file:rounded-full file:border-0
-                           file:text-sm file:font-semibold
-                           file:bg-green-50 file:text-green-700
-                           hover:file:bg-green-100"
+                        file:rounded-full file:border-0
+                        file:text-sm file:font-semibold
+                        file:bg-green-50 file:text-green-700
+                        hover:file:bg-green-100"
       />
       <Button
         onClick={handleFileUpload}
--- expense-tracker/src/pages/WelcomePage.js
+++ expense-tracker/src/pages/WelcomePage.js
@@ -5,29 +5,38 @@ import FileUploader from "../components/FileUploader";
 const WelcomePage = () => {
   const navigate = useNavigate();
   const [file, setFile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onFileUpload = (uploadedFile) => {
     setFile(uploadedFile);
     const categorizedData = [];
-    navigate("/main", { state: { categorizedData } }); // Redirect to MainPage after processing
+    navigate("/main", { state: { categorizedData } });
   };
 
   const onFileUploadError = (errorMessage) => {
     console.error(errorMessage);
+    setIsLoading(false);
+  };
+
+  const onFileUploadStart = () => {
+    setIsLoading(true);
   };
 
   return (
-    <div class="flex justify-center items-center h-screen bg-gray-100">
-      <div class="text-center p-8 bg-white rounded-lg shadow-xl">
-        <p class="text-lg md:text-2xl lg:text-3xl font-bold text-green-600 mb-4">
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="text-center p-8 bg-white rounded-lg shadow-xl">
+        <p className="text-lg md:text-2xl lg:text-3xl font-bold text-green-600 mb-4">
           Hello, Welcome!
         </p>
-        <p class="text-sm md:text-base lg:text-lg text-gray-600 mb-6">
-          Please upload your bank statement file.
+        <p className="text-sm md:text-base lg:text-lg text-gray-600 mb-6">
+          {isLoading
+            ? "Please wait, AI is analyzing your data"
+            : "Please upload your bank statement file."}
         </p>
         <FileUploader
           onFileSelectSuccess={onFileUpload}
           onFileSelectError={onFileUploadError}
+          onFileUploadStart={onFileUploadStart}
         />
       </div>
     </div>
