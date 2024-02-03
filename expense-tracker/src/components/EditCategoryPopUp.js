32c6ef580b9c532f58dd0076ec9afecd2ff6e90e
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-02-03 19:57:38 +0200

    updated styling

--- expense-tracker/src/components/EditCategoryPopUp.js
+++ expense-tracker/src/components/EditCategoryPopUp.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Make sure to import Bootstrap CSS
 
 const EditCategoryPopup = ({
   onClose,
@@ -10,22 +11,47 @@ const EditCategoryPopup = ({
     selectedPurchase.category
   );
 
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   return (
-    <div className="popup">
-      <select
-        value={selectedCategory}
-        onChange={(e) => setSelectedCategory(e.target.value)}
-      >
-        {categories.map((category, index) => (
-          <option key={index} value={category}>
-            {category}
-          </option>
-        ))}
-      </select>
-      <button onClick={() => onSave(selectedCategory, selectedPurchase)}>
-        Save
-      </button>
-      <button onClick={onClose}>Close</button>
+    <div
+      className="popup d-flex justify-content-center align-items-center position-fixed  w-100 h-100"
+      style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+    >
+      <div className="card" style={{ width: "20rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">Edit Category</h5>
+          <select
+            className="form-select"
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value)}
+          >
+            {categories.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+          <div className="mt-3 d-flex justify-content-end">
+            <button
+              className="btn me-2"
+              style={{ backgroundColor: "#48bb78", color: "white" }}
+              onClick={() => onSave(selectedCategory, selectedPurchase)}
+            >
+              Save
+            </button>
+            <button className="btn btn-secondary" onClick={onClose}>
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
