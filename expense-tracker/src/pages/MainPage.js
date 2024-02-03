c6469df13091060920feabc26b816a34e7415ceb
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-02-04 00:33:21 +0200

    minor change

--- expense-tracker/src/pages/MainPage.js
+++ expense-tracker/src/pages/MainPage.js
@@ -370,7 +370,9 @@ const MainPage = () => {
               <div key={index} className="card mb-2">
                 <div className="card-body d-flex justify-content-between align-items-center">
                   <div className="col-4">
-                    <strong>{purchase.businessName}</strong>
+                    <strong>
+                      {financial ? purchase.businessName : purchase.payer}
+                    </strong>
                   </div>
                   <div className="col-4 d-flex justify-content-end">
                     <button
