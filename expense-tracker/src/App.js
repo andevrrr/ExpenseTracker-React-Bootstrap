1d278fa949161b51a37c2b89ace58fe5c7a48f61
Anton Kucherenko <anton.kucherenko@tuni.fi>
2024-02-03 23:09:06 +0200

    added new font

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
 import MainPage from "./pages/MainPage";
+import './App.css';
 
 const App = () => {
   return (
